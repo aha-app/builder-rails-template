d7d993f498b10b4b788a42e294e01f09971c4673
Jeremy Geros <jeremy453@gmail.com>
2025-11-12 15:50:39 -0600

    Linting and formatting

--- app/frontend/entrypoints/inertia.ts
+++ app/frontend/entrypoints/inertia.ts
@@ -1,53 +1,54 @@
-import { createInertiaApp } from '@inertiajs/react';
-import { createElement, ReactNode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from "@inertiajs/react";
+import type { ReactNode } from "react";
+import { createElement } from "react";
+import { createRoot } from "react-dom/client";
 
 // Temporary type definition, until @inertiajs/react provides one
-type ResolvedComponent = {
-	default: ReactNode;
-	layout?: (page: ReactNode) => ReactNode;
-};
+interface ResolvedComponent {
+  default: ReactNode;
+  layout?: (page: ReactNode) => ReactNode;
+}
 
-const appName = import.meta.env.VITE_APP_NAME ?? 'Builder';
+const appName = import.meta.env.VITE_APP_NAME ?? "Builder";
 
-createInertiaApp({
-	// Set default page title
-	// see https://inertia-rails.dev/guide/title-and-meta
-	//
-	title: (title) => (title ? `${title} - ${appName}` : appName),
+void createInertiaApp({
+  // Set default page title
+  // see https://inertia-rails.dev/guide/title-and-meta
+  //
+  title: (title) => (title ? `${title} - ${appName}` : appName),
 
-	// Disable progress bar
-	//
-	// see https://inertia-rails.dev/guide/progress-indicators
-	// progress: false,
+  // Disable progress bar
+  //
+  // see https://inertia-rails.dev/guide/progress-indicators
+  // progress: false,
 
-	resolve: (name) => {
-		const pages = import.meta.glob<ResolvedComponent>('../pages/**/*.tsx', {
-			eager: true,
-		});
-		const page = pages[`../pages/${name}.tsx`];
-		if (!page) {
-			console.error(`Missing Inertia page component: '${name}.tsx'`);
-		}
+  resolve: (name) => {
+    const pages = import.meta.glob<ResolvedComponent>("../pages/**/*.tsx", {
+      eager: true,
+    });
+    const page = pages[`../pages/${name}.tsx`];
+    if (!page) {
+      console.error(`Missing Inertia page component: '${name}.tsx'`);
+    }
 
-		// To use a default layout, import the Layout component
-		// and use the following line.
-		// see https://inertia-rails.dev/guide/pages#default-layouts
-		//
-		// page.default.layout ||= (page) => createElement(Layout, null, page)
+    // To use a default layout, import the Layout component
+    // and use the following line.
+    // see https://inertia-rails.dev/guide/pages#default-layouts
+    //
+    // page.default.layout ||= (page) => createElement(Layout, null, page)
 
-		return page;
-	},
+    return page;
+  },
 
-	setup({ el, App, props }) {
-		if (el) {
-			createRoot(el).render(createElement(App, props));
-		} else {
-			console.error(
-				'Missing root element.\n\n' +
-					'If you see this error, it probably means you load Inertia.js on non-Inertia pages.\n' +
-					'Consider moving <%= vite_typescript_tag "inertia" %> to the Inertia-specific layout instead.'
-			);
-		}
-	},
+  setup({ el, App, props }) {
+    if (el) {
+      createRoot(el).render(createElement(App, props));
+    } else {
+      console.error(
+        "Missing root element.\n\n" +
+          "If you see this error, it probably means you load Inertia.js on non-Inertia pages.\n" +
+          'Consider moving <%= vite_typescript_tag "inertia" %> to the Inertia-specific layout instead.',
+      );
+    }
+  },
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
-import Rails from 'vite-plugin-rails';
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import Rails from "vite-plugin-rails";
 
 export default defineConfig({
-	plugins: [
-		react({
-			babel: {
-				plugins: ['babel-plugin-react-compiler'],
-			},
-		}),
-		tailwindcss(),
-		Rails({
-			envVars: { RAILS_ENV: 'development' },
-		}),
-	],
+  plugins: [
+    react({
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+    tailwindcss(),
+    Rails({
+      envVars: { RAILS_ENV: "development" },
+    }),
+  ],
 });
