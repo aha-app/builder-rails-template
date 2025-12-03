44309571df46cd78720f33eab285905c28766509
Jeremy Geros <jeremy453@gmail.com>
2025-12-03 13:29:33 -0600

    Remove container

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,35 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import EnvironmentPlugin from 'vite-plugin-environment';
-import ViteRuby from 'vite-plugin-ruby';
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import EnvironmentPlugin from "vite-plugin-environment"
+import ViteRuby from "vite-plugin-ruby"
 
 export default defineConfig({
-	plugins: [
-		react({
-			babel: {
-				plugins: ['babel-plugin-react-compiler'],
-			},
-		}),
-		tailwindcss(),
-		ViteRuby(),
-		EnvironmentPlugin({ RAILS_ENV: 'development' }),
-	],
-	server: {
-		hmr: false,
-	},
-	logLevel: 'error',
-	build: {
-		reportCompressedSize: false,
-		rollupOptions: {
-			onwarn(warning, warn) {
-				// Suppress specific warnings
-				if (warning.code === 'EVAL') return;
-				warn(warning);
-			},
-		},
-	},
-});
+  plugins: [
+    react({
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+    tailwindcss(),
+    ViteRuby(),
+    EnvironmentPlugin({ RAILS_ENV: "development" }),
+  ],
+  define: {
+    __REACT_DEVTOOLS_GLOBAL_HOOK__: "({ isDisabled: true })",
+  },
+  server: {
+    hmr: false,
+  },
+  logLevel: "error",
+  build: {
+    reportCompressedSize: false,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Suppress specific warnings
+        if (warning.code === "EVAL") return
+        warn(warning)
+      },
+    },
+  },
+})
