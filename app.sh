b9bd6422729ba3cd092ba8f898075010076536ff
Jeremy Geros <jeremy453@gmail.com>
2026-01-05 10:32:26 -0600

    Remove bundle and npm install

--- app.sh
+++ app.sh
@@ -4,11 +4,6 @@ set -e
 export NODE_ENV=development
 export RAILS_ENV=development
 
-# Ensure configuration is up to date
-bundle install
-npm install
-bin/rails db:migrate
-
 echo "Starting processes (Rails, Vite)"
 
 # Start Vite dev server in the background
